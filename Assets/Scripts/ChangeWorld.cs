10f4c0fe52d05d29f71ce043c3ec082d1156e660
ahmad aljebroi <ahmad.aljebori1@education.nsw.gov.au>
2019-09-09 14:00:21 +1000

    to work on laptop

---
+++ Assets/Scripts/ChangeWorld.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeWorld : MonoBehaviour
+{
+    public string TaregtScene;
+    public void NewScene()
+    {
+        SceneManager.LoadScene(TargetScene, LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    public void SetMenuActive()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
